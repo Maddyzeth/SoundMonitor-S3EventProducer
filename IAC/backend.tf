eaa3f78e4b41e273c10483e20f5e263ec0dccc5c
Maddyzeth <40778985+Maddyzeth@users.noreply.github.com>
2021-12-15 18:12:51 -0500

    Update backend.tf

--- IAC/backend.tf
+++ IAC/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-monitor-provide-states-files"
+    bucket  = "terraform-monitor-provide-states-files182"
     key     = "s3event.tfstate"
     region  = "us-east-1"
   }
